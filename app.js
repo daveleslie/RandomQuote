e18fc2ac04f6374eca1e47503f5d019af51b5f8c
daveleslie <dave.leslie.online@gmail.com>
2021-10-13 17:55:50 +0200

    Added newQuote method

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+// const {quotes} = require("./quotes.js")
+
 const randomQuote = {
   data() {
     return {
-      quotes: Seed.quotes,
-      test: "some text"
+      quotes,
+      displayedQuote: quotes[0],
+      test: 'test',
+      tweetLink: "http://www.twitter.com/intent/tweet?text="+this.displayedQuote.quote
+    }
+  },
+  methods: {
+    newQuote() {
+      let randomNumber = Math.floor(Math.random() * quotes.length)
+      this.displayedQuote = quotes[randomNumber]
     }
   }
-
 }
 
 Vue.createApp(randomQuote).mount('#app') 
