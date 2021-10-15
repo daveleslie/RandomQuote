263f32840b4ed34e1758d98d8c6cd84c8b9552fe
daveleslie <dave.leslie.online@gmail.com>
2021-10-15 09:38:17 +0200

    Added async getQuotes method

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
-// const {quotes} = require("./quotes.js")
-import { quotes } from "./quotes.js"
-
 const randomQuote = {
   data() {
     return {
-      quotes,
-      displayedQuote: quotes[0],
-      test: 'test',
-      tweetLink: 'http://www.twitter.com/intent/tweet?text='
-    }
+      quoteArr: [],
+      quote: "",
+      author: "",
+      tweetLink: "http://www.twitter.com/intent/tweet?text=",
+    };
   },
   computed: {
     tweetQuote() {
-      return `${this.displayedQuote.quote} - ${this.displayedQuote.author}`
-    }
+      return `${this.quote} - ${this.author}`;
+    },
   },
   methods: {
+    async getQuotes() {
+      try {
+        let response = await fetch(
+          "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/"
+        );
+        this.quoteArr = await response.json();
+        let randomNumber = Math.floor(
+          Math.random() * this.quoteArr.quotes.length
+        );
+        this.quote = this.quoteArr.quotes[randomNumber].quote;
+        this.author = this.quoteArr.quotes[randomNumber].author;
+      } catch (error) {
+        console.log(error);
+      }
+    },
     newQuote() {
-      let randomNumber = Math.floor(Math.random() * quotes.length)
-      this.displayedQuote = quotes[randomNumber]
-    }
-  }
-}
+      let randomNumber = Math.floor(
+        Math.random() * this.quoteArr.quotes.length
+      );
+      this.quote = this.quoteArr.quotes[randomNumber].quote;
+      this.author = this.quoteArr.quotes[randomNumber].author;
+    },
+  },
+  created() {
+    this.getQuotes();
+  },
+};
 
-Vue.createApp(randomQuote).mount('#app') 
+Vue.createApp(randomQuote).mount("#app");
